22fa633336321ce7d4755c8b6d087789ddf5797e
danchitnis <danchitnis@users.noreply.github.com>
2023-01-20 17:08:42 +0000

    vispy

--- pyglplot/pyglplot.py
+++ pyglplot/pyglplot.py
@@ -6,92 +6,77 @@ In addition we test if instancing is working passing gl_InstanceID from the vert
 
 # https://stackoverflow.com/questions/63086665/how-to-set-primitives-to-gl-lines-with-moderngl-in-python
 
-import moderngl
+from vispy import gloo, app
+from vispy.gloo import Program
 import numpy as np
-import moderngl_window as mglw
+
+class Hello:
+    def __init__(self):
+        print("Hello")
 
 
-class Pyglplot(mglw.WindowConfig):
-    gl_version = (3, 3)
-    title = "pyglplot"
-    window_size = (1280, 720)
-    aspect_ratio = 16 / 9
-    resizable = True
+class Pyglplot(app.Canvas):
+    def __init__(self):
+        super().__init__(size=(512, 512), title='Rotating quad',
+                         keys='interactive')
+        # Build program & data
+        self._vertex = """
+            #version 330
+            attribute vec2 coordinates;
+                        
+            uniform mat2 uScale;
+            uniform vec2 uOffset;
+                        
+            void main(void) {
+                vec2 line = uScale * coordinates + uOffset;
+                gl_Position = vec4(line, 0.0, 1.0);
+            }
+            """
 
-    def __init__(self, **kwargs,):
-        super().__init__(**kwargs)
+        self._fragment = """
+            #version 330
+            uniform vec3 pColor;
+            void main(void) {
+                gl_FragColor =  vec4(pColor, 1);
+            }
+            """
 
-        self.prog = self.ctx.program(
-            vertex_shader='''
-                #version 330
-                in vec2 coordinates;
-                
-                uniform mat2 uScale;
-                uniform vec2 uOffset;
-                
-                void main(void) {
-                    vec2 line = uScale * coordinates + uOffset;
-                    gl_Position = vec4(line, 0.0, 1.0);
-                }
-            ''',
+        self._x = np.array([-1, 1])
+        self._y = np.array([1, -1])
+        self._rgb = np.array([1, 0.5, 0.5])
 
-            fragment_shader='''
-                #version 330
-                uniform vec3 pColor;
-                void main(void) {
-                    gl_FragColor =  vec4(pColor, 1);
-                }
-            ''',
-        )
+        self._program = Program(self._vertex, self._fragment)
+        self._program['pColor'] = self._rgb
+        self._program['coordinates'] = np.dstack([self._x,self._y]).astype('f4')
+        self._program['uOffset'] = [0.0, 0.0]
+        self._program['uScale'] = np.array([1.0, 0, 0, 1])
 
-        self.x = np.array([0, 0])
-        self.y = np.array([1, 1])
+        gloo.set_viewport(0, 0, *self.physical_size)
+        gloo.set_clear_color('black')
 
-        self.rgb = np.array([1, 1, 1])
-        
-        self.init()
+        self.timer = app.Timer('auto', self.on_timer)
+        self.clock = 0
+        self.timer.start()
 
-        self.doStuff()
+        self.show()
 
-        #self.vertices = np.array([[-1.0, -1.0, 0.6, 0.8]])
+    def on_draw(self, event):
+        gloo.clear()
+        self._program.draw('line_strip')
 
-    def init(self):
-        self.x = np.array([0, 0])
-        self.y = np.array([1, 1])
-        self.rgb = np.array([1, 1, 1])
+    def on_resize(self, event):
+        gloo.set_viewport(0, 0, *event.physical_size)
 
     
+    def setXY(self, x: np.ndarray , y: np.ndarray):
+        self._x = x
+        self._y = y
+        self._program['coordinates'] = np.dstack([self._x,self._y]).astype('f4')
     
-    def setXY(self):    
-        self.x = np.array([0, 0])
-        self.y = np.array([1, 1])
+    def eventLoop(self):
+        pass
 
-   
-
-    def doStuff(self):
-        self.vertices = np.dstack([self.x, self.y])
-        self.vbo = self.ctx.buffer(self.vertices.astype('f4').tobytes())
-        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'coordinates')
-
-        self.vertices2 = np.dstack([self.y, self.x])
-        self.vbo2 = self.ctx.buffer(self.vertices2.astype('f4').tobytes())
-        self.vao2 = self.ctx.simple_vertex_array(self.prog, self.vbo2, 'coordinates',)
-
-        self.prog["pColor"].value = self.rgb
-        self.prog["uScale"].value = 1.0, 0.5, 0, 0.5
-        self.prog["uOffset"].value = 0.35, 0
-
-        
-
-    def render(self, time, frame_time):
-        self.ctx.clear(0.0, 0.0, 0.0)
-
-        self.setXY()
-        self.doStuff()
-
-        self.vao.render(moderngl.LINE_STRIP)
-        self.vao2.render(moderngl.LINE_STRIP)
-        
-
-    def resize(self, width: int, height: int):
-        print("Window was resized. buffer size is {} x {}".format(width, height))
+    def on_timer(self, event):
+        self.eventLoop()
+        self._program['pColor'] = self._rgb
+        self.update()
